eb57a9de3d571b89e6db2c87da8144625d48a114
laryssaantunes <larissa.antunes.icloud@gmail.com>
2024-10-02 07:54:28 -0400

    commit adicionei o script.js

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-
-
+const buttons = document.getElementsByTagName('button');
+for (let i = 0; i < buttons.length; i++) {buttons[i].onclick = function() {alert('Você clicou em "Saiba Mais"!');
+     buttons[i].textContent = 'Obrigado por clicar!';
+     buttons[i].disabled = true;
+    };
+}
